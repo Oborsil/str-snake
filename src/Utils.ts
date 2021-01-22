683fb078c165682aaa0e785d56d20d4ca70db8ee
Mercédesz Anett Porkoláb <09dporkolabm@edu.bme.hu>
2021-01-22 21:41:30 +0100

    Első feladat kész.

--- src/Utils.ts
+++ src/Utils.ts
@@ -11,7 +11,20 @@ class Debouncer {
      * @returns {number} - válaszd ki a kisebbet a num és a max közül 
      * majd válaszd ki a nagyobbat az előbbi érték és a min közül.
      */
-    
+    static bound(num: number, min: number, max: number) {
+        let less: number;
+        if (num < max) { 
+            less = num;
+        } else {
+            less= max;
+        }
+
+        if (less <min) {
+            return min;
+        } else {
+            return less;
+        }
+    } 
 
     static debounce<T extends Function>(fun: T, wait: number) {
         let id: any;
